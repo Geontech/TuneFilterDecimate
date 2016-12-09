ec46bd8211b65b7f59429e7cce70401287ffe2ee
Patrick Wolfram <pwolfram@geontech.com>
2016-12-09 17:00:50 -0500

    Overriding releaseObject to prevent the singleton event manager from being killed on component release.

--- cpp_rfnoc/TuneFilterDecimate.cpp
+++ cpp_rfnoc/TuneFilterDecimate.cpp
@@ -264,6 +264,21 @@ void TuneFilterDecimate_i::stop() throw (CF::Resource::StopError, CORBA::SystemE
     TuneFilterDecimate_base::stop();
 }
 
+void TuneFilterDecimate_i::releseObject() throw (CF::LifeCycle::ReleaseError, CORBA::SystemException)
+{
+    LOG_TRACE(TuneFilterDecimate_i, __PRETTY_FUNCTION__);
+
+    releasePorts();
+    stopPropertyChangeMonitor();
+    // This is a singleton shared by all code running in this process
+    //redhawk::events::Manager::Terminate();
+    PortableServer::POA_ptr root_poa = ossie::corba::RootPOA();
+    PortableServer::ObjectId_var oid = root_poa->servant_to_id(this);
+    root_poa->deactivate_object(oid);
+
+    component_running.signal();
+}
+
 /*
  * A method which allows a callback to be set for the block ID changing. This
  * callback should point back to the persona to alert it of the component's
